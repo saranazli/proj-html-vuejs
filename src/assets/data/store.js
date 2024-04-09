f534dc502eb34098646057e0e49e44dad916d32c
s4r44d43 <sararez@live.it>
2024-04-09 10:58:59 +0200

    [start]

--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -3,5 +3,6 @@ import { reactive } from "vue";
 
 export const store = reactive ({
  
-  
+  phoneNumber: '1.800.123.4567',
+
 })
--- src/components/partials/Header_components/Nav_top.vue
+++ src/components/partials/Header_components/Nav_top.vue
@@ -19,25 +19,29 @@
 
 <template>
   
-  <section class="container d-flex justify-content-center justify-content-between align-items-center  cs_header">
+  <section class="cs_header">
+
+    <div class="container d-flex justify-content-center justify-content-between align-items-center h-100">
       
-    <div class="logo_front">
-      <img src="/public/img/dark-pet-logo-x.png" alt="logo">
-    </div>
-
-    <div>
-      <i class="fa-solid fa-magnifying-glass"></i>
-      <input type="text" placeholder="search...">
-    </div>
-
-    <div>
-      <span>Questions</span>
-      <span>Call us: 1.800.123.4567</span>
-    </div>
-
-    <div>
-      <i class="fa-solid fa-bag-shopping"></i>
-      <i class="fa-solid fa-user"></i>
+      <div class="logo_front">
+        <img src="/img/dark-pet-logo-x.png" alt="logo">
+      </div>
+  
+      <div>
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder="search...">
+      </div>
+  
+      <div>
+        <span>Questions ? </span>
+        <span>Call us: {{ store.phoneNumber }}</span>
+      </div>
+  
+      <div>
+        <i class="fa-solid fa-bag-shopping"></i>
+        <i class="fa-solid fa-user"></i>
+      </div>
+  
     </div>
 
   </section>
@@ -52,6 +56,7 @@
   
   .cs_header{
     height: 75px;
+    border-bottom: 0.5px solid grey ;
     .logo_front{
       width: 240px;
       height: 40px;
