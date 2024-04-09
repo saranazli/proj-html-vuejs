d85e5eee4b0c175ee1edf4e6fdcaab062532d747
s4r44d43 <sararez@live.it>
2024-04-09 17:00:08 +0200

    [end] top jumbo

--- src/components/partials/Jumbo_comp/Top_j.vue
+++ src/components/partials/Jumbo_comp/Top_j.vue
@@ -14,8 +14,50 @@
 </script>
 
 <template>
-  <section class="top_j">
+  <section class="top_j d-flex center">
+    <div class="container-xl d-flex around center">
 
+      <div>
+        <span class="me-2">
+          <i class="fa-regular fa-map"></i>
+        </span>
+        <span class="bold">
+          International
+        </span>
+        <span>
+          Shipping Service
+        </span>
+      </div>
+
+      <div>
+        <span class="me-2">
+          <i class="fa-regular fa-clock"></i>
+        </span>
+        <span>
+          See our
+        </span>
+        <span class="bold">
+          store hours
+        </span>
+        <span>
+          and service
+        </span>
+
+      </div>
+
+      <div>
+        <span class="me-2">
+          <i class="fa-solid fa-check"></i>
+        </span>
+        <span>
+          We price match to give you the
+        </span>
+        <span class="bold">
+          best deals
+        </span>
+      </div>
+
+    </div>
   </section>
 </template>
 
@@ -27,6 +69,8 @@
     height: 65px;
     background-color: $heavy_metal_green;
     background-image: url(/img/bg-transparent-3.png);
+    color: white;
+    font-size: 0.75rem;
   }
 
 </style>
